032105cf60f74939b2112a7b75d12c5614bb9458
Ruth-Mwangi <rwmwangi96@gmail.com>
2023-12-14 19:44:33 +0300

    Added a common response class to standardize response from API

---
+++ src/main/java/com/ruth/rurucraftsecommerce/response/Response.java
@@ -0,0 +1,40 @@
+package com.ruth.rurucraftsecommerce.response;
+
+public class Response {
+    private String statusCode;
+    private String message;
+    private Object data;
+
+    public Response() {
+    }
+
+    public Response(String statusCode, String message, Object data) {
+        this.statusCode = statusCode;
+        this.message = message;
+        this.data = data;
+    }
+
+    public String getStatusCode() {
+        return statusCode;
+    }
+
+    public void setStatusCode(String statusCode) {
+        this.statusCode = statusCode;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public Object getData() {
+        return data;
+    }
+
+    public void setData(Object data) {
+        this.data = data;
+    }
+}
