af2c2cb22c4368f88a7e500e06d4e5de98eb8686
Ruth-Mwangi <rwmwangi96@gmail.com>
2023-12-18 15:17:15 +0300

    Create unit tests for permissions.

--- src/main/java/com/ruth/rurucraftsecommerce/permissions/Permission.java
+++ src/main/java/com/ruth/rurucraftsecommerce/permissions/Permission.java
@@ -4,6 +4,7 @@ import com.ruth.rurucraftsecommerce.common.BaseEntity;
 import jakarta.persistence.*;
 import org.springframework.security.core.GrantedAuthority;
 
+import java.util.Date;
 import java.util.Set;
 
 @Entity
@@ -13,6 +14,18 @@ public class Permission  extends BaseEntity {
     @Column(nullable = false,unique = true)
     private String name;
 
+    public Permission(Integer id,String name) {
+        this.setId(id);
+        this.name = name;
+    }
+
+    public Permission(Integer id, String name, Date deletedAt) {
+        this.setDeletedAt(deletedAt);
+        this.setId(id);
+        this.name = name;
+    }
+    public Permission() {
+    }
 
     public String getName() {
         return name;
